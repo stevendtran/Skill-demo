a268d0d7fb1798d5377ce8d26cf106d466c6723b
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2022-04-29 17:23:38 -0700

    New changes

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,5 +1,12 @@
-public class SkillDemo(){
-
-//Insert code here.
-
+public class SkillDemo{
+    static double conversionK = 0.453592;
+ 
+    public static double convertLbs(double lbs){
+        return lbs * conversionK;
+    }
+ 
+    public static double convertKgs(double kgs){
+        return kgs / conversionK;
+    }
 }
+
---
+++ SkillDemoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+ 
+public class SkillDemoTest{
+    @Test
+    public void LbstoKgsTest(){
+        assertEquals(45.3592, SkillDemo.convertLbs(100));
+    }
+ 
+    @Test
+    public void KgsToLbsTest(){
+        assertEquals(3.3069, SkillDemo.convertKgs(1.5), 4);
+    }
+}
+
